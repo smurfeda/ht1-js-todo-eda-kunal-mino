9c76de7d9f818eafe3c6a7ffb1e15cc49a93e5de
eda <edaburns@gmail.com>
2022-09-01 09:55:17 +0100

    notes to explain logic

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const data = [
   { id: 1, todo: "go to Hampstead Heath" },
-  { id: 2, todo: "play with ;,,l;,;l,Kabir" },
+  { id: 2, todo: "play with Kabir" },
 ];
 
 document.title = "My Todo List";
@@ -19,12 +19,18 @@ function handleClick() {
   console.log("this is working");
   let ul = document.querySelector("#list");
   let inputValue = document.querySelector("#todoField").value;
-  data.push(inputValue);
+//   data.push(inputValue);
 
   let newLi = document.createElement("li");
   newLi.innerText = inputValue;
   ul.appendChild(newLi);
 }
+// What the function is doing: Lines 18- 27
+// this function is called to action in the button section in HTML file
+// We are getting the value from the user input and then displaying as li
+// When we click the button, we are selecting the ul with the id 'list' and 
+// grabbing the input from the user and storing it in inputValue 
+// And then adding the new li under the ul
 
 let ul = document.querySelector("#list");
 data.map(function (item) {
@@ -32,3 +38,20 @@ data.map(function (item) {
   newLi.innerText = item.todo;
   ul.appendChild(newLi);
 });
+
+//Below is the refactored code of map method with the for loop
+
+// for (i = 0; i < data.length ; i++){
+//     let ul = document.querySelector("#list");
+//     let newLi = document.createElement("li");
+//     newLi.innerText = data[i].todo;
+//     ul.appendChild(newLi);
+// }
+
+// We are grabbing the ul with the id 'list'
+// Then we are mapping through the 'data' array with the mappin array method.
+// Map method needs a callback function. Item is the placeholder. 
+// Item is the equivalent of data[i] in the for loop. Items represent each object.
+// We are creating a variable to store the new li element 
+// With the newLi, the inner text shows the todo from the data array
+// Then we are putting the new li underneath the old ul
