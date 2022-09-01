eb67aa73565ae9c8b9d7798eaf6bf7f18dc0d1f3
Kunal S <kunals@Kunals-iMac.local>
2022-09-01 23:22:44 +0100

    tidied up the code

--- main.js
+++ main.js
@@ -1,49 +1,25 @@
-
-// This is redundant for now as we don't have time to make the pushed array
-// item display in the above format
-
-//  const data = ["go to Hampstead Heath", "play with Kabir"]
-
-// const data = []
-
 const data = [
-  // { id: 1, todo: "go to Hampstead Heath" },
-  // { id: 2, todo: "play with Kabir" },
+  { id: 1, todo: "go to Hampstead Heath" },
+  { id: 2, todo: "play with Kabir" },
 ];
 
-document.title = "My Todo List";
-// const input = document.createElement("input");
-// input.type = "text"
-// document.body.appendChild(input);
-
-// const button = document.createElement("button");
-// button.textContent = 'Add Task';
-// // button.innertext = "Add Task"
-// document.body.appendChild(button);
-
-// button.addEventListener("click", handleClick)
 let ul = document.querySelector("#list");
  
 function handleClick() {
   console.log("this is working");
   let ul = document.querySelector("#list");
   let inputValue = document.querySelector("#todoField").value;
-  // data.push(inputValue);
   data.push({id:data.length+1, todo:inputValue})
   console.log("data:", data)
   let newLi = document.createElement("li");
   newLi.innerText = data[data.length-1].todo;
   ul.appendChild(newLi);
 }
-//     data.map(function (item) {
-//     let newLi = document.createElement("li");
-//     newLi.innerText = item;
-//     ul.appendChild(newLi);
-//   });
-
-  //let newLi = document.createElement("li");
-//   newLi.innerText = inputValue;
-//   ul.appendChild(newLi);
+    data.map(function (item) {
+    let newLi = document.createElement("li");
+    newLi.innerText = item.todo;
+    ul.appendChild(newLi);
+  });
 
 
 // What the function is doing: Lines 18- 27
@@ -53,12 +29,6 @@ function handleClick() {
 // grabbing the input from the user and storing it in inputValue 
 // And then adding the new li under the ul
 
-// let ul = document.querySelector("#list");
-//   data.map(function (item) {
-//   let newLi = document.createElement("li");
-//   newLi.innerText = item.todo;
-//   ul.appendChild(newLi);
-// });
 
 //Below is the refactored code of map method with the for loop
 
