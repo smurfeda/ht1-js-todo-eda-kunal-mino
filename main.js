789a5a3533e80a3ec3ad627327a552ae61c7c0b9
Kunal S <kunals@Kunals-iMac.local>
2022-09-02 00:22:41 +0100

    final version to be submitted to Hamza for feedback

--- main.js
+++ main.js
@@ -4,15 +4,18 @@ const data = [
 ];
 
 let ul = document.querySelector("#list");
-
+//mapping through data array and creating a new li for each 'todo' value 
 data.map(function (item) {
   let newLi = document.createElement("li");
   newLi.innerText = item.todo;
   ul.appendChild(newLi);
 });
  
+//on click function which captures the value inputted in text field. 
+// we are then pushing the user input into the array as an object
+//we are then creating a li with the newly added object as the inner text and appending it the the ul tag with id '#list'
+//the function is called in the 'Add Todo' button tag (line 14, index.html)
 function handleClick() {
-  console.log("this is working");
   let ul = document.querySelector("#list");
   let inputValue = document.querySelector("#todoField").value;
   data.push({id:data.length+1, todo:inputValue})
@@ -21,30 +24,3 @@ function handleClick() {
   newLi.innerText = data[data.length-1].todo;
   ul.appendChild(newLi);
 }
-   
-
-
-// What the function is doing: Lines 18- 27
-// this function is called to action in the button section in HTML file
-// We are getting the value from the user input and then displaying as li
-// When we click the button, we are selecting the ul with the id 'list' and 
-// grabbing the input from the user and storing it in inputValue 
-// And then adding the new li under the ul
-
-
-//Below is the refactored code of map method with the for loop
-
-// for (i = 0; i < data.length ; i++){
-//     let ul = document.querySelector("#list");
-//     let newLi = document.createElement("li");
-//     newLi.innerText = data[i].todo;
-//     ul.appendChild(newLi);
-// }
-
-// We are grabbing the ul with the id 'list'
-// Then we are mapping through the 'data' array with the mappin array method.
-// Map method needs a callback function. Item is the placeholder. 
-// Item is the equivalent of data[i] in the for loop. Items represent each object.
-// We are creating a variable to store the new li element 
-// With the newLi, the inner text shows the todo from the data array
-// Then we are putting the new li underneath the old ul
